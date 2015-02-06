d916fd1ad3978c1e9cbd5b5634364bec05062600
marcin <marcinq3@o2.pl>
2015-02-06 21:13:40 +0100

    first commit

---
+++ archmenu.lua
@@ -0,0 +1,102 @@
+ local menu98edb85b00d9527ad5acebe451b3fae6 = {
+     {"7-Zip FM", "7zFM", "/usr/share/icons/hicolor/32x32/apps/p7zip.png" },
+     {"Akonaditray", "akonaditray", "/usr/share/icons/hicolor/22x22/apps/akonaditray.png" },
+     {"Ark", "ark -caption Ark ", "/usr/share/icons/hicolor/16x16/apps/ark.png" },
+     {"KCalc", "kcalc -caption KCalc", "/usr/share/icons/gnome/16x16/apps/accessories-calculator.png" },
+     {"KWrite", "kwrite ", "/usr/share/icons/gnome/16x16/apps/accessories-text-editor.png" },
+     {"Kate", "kate -b ", "/usr/share/icons/hicolor/16x16/apps/kate.png" },
+     {"Krusader", "krusader -caption \"Krusader\" ", "/usr/share/icons/hicolor/16x16/apps/krusader_user.png" },
+     {"Qemu Launcher", "qemu-launcher"},
+ }
+
+ local menud334dfcea59127bedfcdbe0a3ee7f494 = {
+     {"Gwenview", "gwenview ", "/usr/share/icons/hicolor/16x16/apps/gwenview.png" },
+     {"KColorChooser", "kcolorchooser", "/usr/share/icons/hicolor/16x16/apps/kcolorchooser.png" },
+     {"KRuler", "kruler", "/usr/share/icons/hicolor/16x16/apps/kruler.png" },
+     {"KSnapshot", "ksnapshot -caption KSnapshot", "/usr/share/icons/hicolor/16x16/apps/ksnapshot.png" },
+     {"KolourPaint", "kolourpaint ", "/usr/share/icons/hicolor/16x16/apps/kolourpaint.png" },
+     {"Okular", "okular   -caption Okular", "/usr/share/icons/hicolor/16x16/apps/okular.png" },
+ }
+
+ local menuc8205c7636e728d448c2774e6a4a944b = {
+     {"Akregator", "akregator   -caption Akregator", "/usr/share/icons/hicolor/16x16/apps/akregator.png" },
+     {"Avahi SSH Server Browser", "/usr/bin/bssh", "/usr/share/icons/gnome/16x16/devices/network-wired.png" },
+     {"Avahi VNC Server Browser", "/usr/bin/bvnc", "/usr/share/icons/gnome/16x16/devices/network-wired.png" },
+     {"BlueDevil", "bluedevil-monolithic"},
+     {"Chromium", "chromium ", "/usr/share/icons/hicolor/16x16/apps/chromium.png" },
+     {"Firefox", "/usr/lib/firefox/firefox ", "/usr/share/icons/hicolor/16x16/apps/firefox.png" },
+     {"HipChat", "/opt/HipChat/bin/hipchat", "/usr/share/icons/hicolor/16x16/apps/hipchat.png" },
+     {"KRDC", "krdc -caption KRDC "},
+     {"KTorrent", "ktorrent  -caption \"KTorrent\" ", "/usr/share/icons/hicolor/16x16/apps/ktorrent.png" },
+     {"Konqueror", "kfmclient openProfile webbrowsing", "/usr/share/icons/hicolor/16x16/apps/konqueror.png" },
+     {"PIM Setting Exporter", "pimsettingexporter"},
+     {"SSH AskPass", "/usr/bin/ssh-add"},
+     {"Skype", "skype ", "/usr/share/icons/hicolor/16x16/apps/skype.png" },
+     {"Thunderbird", "thunderbird ", "/usr/share/icons/hicolor/16x16/apps/thunderbird.png" },
+     {"X2Go Client", "x2goclient", "/usr/share/icons/hicolor/16x16/apps/x2goclient.png" },
+     {"Zenmap", "zenmap ", "///usr/share/zenmap/pixmaps/zenmap.png" },
+     {"Zenmap (as root)", "/usr/share/zenmap/su-to-zenmap.sh ", "///usr/share/zenmap/pixmaps/zenmap.png" },
+ }
+
+ local menudf814135652a5a308fea15bff37ea284 = {
+     {"LibreOffice", "libreoffice ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-startcenter.png" },
+     {"LibreOffice Calc", "libreoffice --calc ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-calc.png" },
+     {"LibreOffice Writer", "libreoffice --writer ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-writer.png" },
+     {"Okular", "okular   -caption Okular", "/usr/share/icons/hicolor/16x16/apps/okular.png" },
+     {"PIM Setting Exporter", "pimsettingexporter"},
+ }
+
+ local menue6f43c40ab1c07cd29e4e83e4ef6bf85 = {
+     {"Qt Assistant", "/usr/lib/qt/bin/assistant", "/usr/share/icons/hicolor/32x32/apps/assistant.png" },
+     {"Qt Designer", "/usr/lib/qt/bin/designer", "/usr/share/icons/hicolor/128x128/apps/designer.png" },
+     {"Qt Linguist", "/usr/lib/qt/bin/linguist", "/usr/share/icons/hicolor/16x16/apps/linguist.png" },
+     {"Qt QDbusViewer ", "/usr/lib/qt/bin/qdbusviewer", "/usr/share/icons/hicolor/32x32/apps/qdbusviewer.png" },
+     {"Qt4 Assistant ", "assistant-qt4", "/usr/share/icons/hicolor/32x32/apps/assistant-qt4.png" },
+     {"Qt4 Designer", "designer-qt4", "/usr/share/icons/hicolor/128x128/apps/designer-qt4.png" },
+     {"Qt4 Linguist ", "linguist-qt4", "/usr/share/icons/hicolor/16x16/apps/linguist-qt4.png" },
+     {"Qt4 QDbusViewer ", "qdbusviewer-qt4", "/usr/share/icons/hicolor/32x32/apps/qdbusviewer-qt4.png" },
+ }
+
+ local menu52dd1c847264a75f400961bfb4d1c849 = {
+     {"Clementine", "clementine ", "/usr/share/icons/hicolor/64x64/apps/application-x-clementine.png" },
+     {"Dragon Player", "dragon ", "/usr/share/icons/hicolor/16x16/apps/dragonplayer.png" },
+     {"GNOME MPlayer", "gnome-mplayer ", "/usr/share/icons/hicolor/16x16/apps/gnome-mplayer.png" },
+     {"JuK", "juk -caption JuK ", "/usr/share/icons/hicolor/16x16/apps/juk.png" },
+     {"K3b", "k3b ", "/usr/share/icons/hicolor/16x16/apps/k3b.png" },
+     {"KsCD", "kscd -caption KsCD ", "/usr/share/icons/hicolor/16x16/apps/kscd.png" },
+     {"QNapi", "qnapi", "///usr/share/icons/qnapi-48.png" },
+     {"Qt V4L2 test Utility", "qv4l2", "/usr/share/icons/hicolor/16x16/apps/qv4l2.png" },
+     {"SMPlayer", "smplayer ", "/usr/share/icons/hicolor/16x16/apps/smplayer.png" },
+     {"VLC media player", "/usr/bin/vlc --started-from-file ", "/usr/share/icons/hicolor/16x16/apps/vlc.png" },
+     {"guvcview", "guvcview", "///usr/share/pixmaps/guvcview/guvcview.png" },
+ }
+
+ local menuee69799670a33f75d45c57d1d1cd0ab3 = {
+     {"Avahi Zeroconf Browser", "/usr/bin/avahi-discover", "/usr/share/icons/gnome/16x16/devices/network-wired.png" },
+     {"Bulk Rename", "/usr/lib/Thunar/ThunarBulkRename ", "/usr/share/icons/hicolor/16x16/apps/Thunar.png" },
+     {"Dolphin", "dolphin  -caption Dolphin ", "/usr/share/icons/gnome/16x16/apps/system-file-manager.png" },
+     {"Htop", "xterm -e htop", "/usr/share/pixmaps/htop.png" },
+     {"K3b", "k3b ", "/usr/share/icons/hicolor/16x16/apps/k3b.png" },
+     {"KSysGuard", "ksysguard ", "/usr/share/icons/gnome/16x16/apps/utilities-system-monitor.png" },
+     {"KWalletManager", "kwalletmanager ", "/usr/share/icons/hicolor/16x16/apps/kwalletmanager.png" },
+     {"Konsole", "konsole", "/usr/share/icons/gnome/16x16/apps/utilities-terminal.png" },
+     {"LXTerminal", "lxterminal", "/usr/share/icons/hicolor/128x128/apps/lxterminal.png" },
+     {"QtEmu", "qtemu", "/usr/share/icons/qtemu.png" },
+     {"Terminal", "gnome-terminal", "/usr/share/icons/gnome/16x16/apps/utilities-terminal.png" },
+     {"Thunar File Manager", "thunar ", "/usr/share/icons/hicolor/16x16/apps/Thunar.png" },
+     {"UNetbootin", "/usr/bin/unetbootin_polkit", "/usr/share/icons/hicolor/16x16/apps/unetbootin.png" },
+     {"Virtual Machine Manager", "virt-manager", "/usr/share/icons/hicolor/16x16/apps/virt-manager.png" },
+     {"Wireshark", "wireshark ", "/usr/share/icons/hicolor/16x16/apps/wireshark.png" },
+     {"dconf Editor", "dconf-editor", "/usr/share/icons/hicolor/16x16/apps/dconf-editor.png" },
+ }
+
+xdgmenu = {
+    {"Accessories", menu98edb85b00d9527ad5acebe451b3fae6},
+    {"Graphics", menud334dfcea59127bedfcdbe0a3ee7f494},
+    {"Internet", menuc8205c7636e728d448c2774e6a4a944b},
+    {"Office", menudf814135652a5a308fea15bff37ea284},
+    {"Programming", menue6f43c40ab1c07cd29e4e83e4ef6bf85},
+    {"Sound & Video", menu52dd1c847264a75f400961bfb4d1c849},
+    {"System Tools", menuee69799670a33f75d45c57d1d1cd0ab3},
+}
+
